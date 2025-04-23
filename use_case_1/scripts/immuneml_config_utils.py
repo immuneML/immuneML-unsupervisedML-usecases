f94c34f1f7d338a932b36d8ff63672794e0b74d4
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-04-23 16:26:37 +0200

    Update configs

--- use_case_1/scripts/immuneml_config_utils.py
+++ use_case_1/scripts/immuneml_config_utils.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 
-def write_immuneml_config(input_train_config_template, input_data, output_config_file, gen_examples_count=None):
+def write_immuneml_config(input_train_config_template, input_data, output_config_file, gen_examples_count=None, models=None):
     """Writes an immuneML YAML config by modifying a template."""
     with open(input_train_config_template, 'r') as file:
         template_config = yaml.safe_load(file)
@@ -10,7 +10,7 @@ def write_immuneml_config(input_train_config_template, input_data, output_config
     template_config['definitions']['datasets']['dataset']['params']['path'] = input_data
 
     if gen_examples_count:
-        for model in ["PWM", "LSTM", "VAE"]:
+        for model in models:
             template_config['instructions'][model]['gen_examples_count'] = gen_examples_count
 
     with open(output_config_file, 'w') as file:
