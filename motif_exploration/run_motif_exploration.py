ffbacf122f7d363cd9a7dbc4e429dad5b6bbbe87
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-04-23 16:28:03 +0200

    Add motif frequency exploration for selection in ligo experiment

---
+++ motif_exploration/run_motif_exploration.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from immuneML.app.ImmuneMLApp import ImmuneMLApp
+from immuneML.app.LigoApp import LigoApp
+
+
+def main():
+    # app = LigoApp(specification_path=Path("simulation.yaml"), result_path=Path("simulation_output/"))
+    # app.run()
+
+    app = ImmuneMLApp(specification_path=Path("specs.yaml"), result_path=Path("report_output/"))
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
+
