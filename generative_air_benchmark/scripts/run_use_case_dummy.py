78671063c2b26e16a02ad61cd695bda70215d711
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-05-28 10:57:09 +0200

    uncomment lines

--- generative_air_benchmark/scripts/run_use_case_dummy.py
+++ generative_air_benchmark/scripts/run_use_case_dummy.py
@@ -17,17 +17,17 @@ def main():
     configs_dir = "generative_air_benchmark/configs/dummy_configs"
     output_dir = "generative_air_benchmark/dummy_results"
 
-    # # simulate data
-    # run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
-    #                result_path=f"{output_dir}/00_simulation/")
-    #
-    # # run training
-    # run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
-    #              result_path=f"{output_dir}/01_train/")
-    #
-    # # run filtering
-    # run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
-    #              result_path=f"{output_dir}/02_filter/")
+    # simulate data
+    run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
+                   result_path=f"{output_dir}/00_simulation/")
+
+    # run training
+    run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
+                 result_path=f"{output_dir}/01_train/")
+
+    # run filtering
+    run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
+                 result_path=f"{output_dir}/02_filter/")
 
     # run feature comparison reports
     run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
