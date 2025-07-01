6395d28a314e8380b2d6d4de0965a8669f98eae9
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-07-01 10:40:38 +0200

    update configs to run multiple models and motif summary report

--- generative_air_benchmark/scripts/run_use_case.py
+++ generative_air_benchmark/scripts/run_use_case.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+
+import pandas as pd
 from immuneML.app.LigoApp import LigoApp
 from immuneML.app.ImmuneMLApp import ImmuneMLApp
 
@@ -12,6 +14,29 @@ def run_immuneML(specification_path, result_path):
     app = ImmuneMLApp(specification_path=Path(specification_path), result_path=Path(result_path))
     app.run()
 
+def get_train_and_test_ids(input_data_path, output_path):
+    input_data_df = pd.read_csv(input_data_path, sep="\t")
+
+    if input_data_df["sequence_id"].isnull().all():
+        input_data_df["sequence_id"] = range(1, len(input_data_df) + 1)
+
+    train_ids, test_ids = [], []
+    signals = [f"signal{i}" for i in range(1, 6)]
+    for signal in signals:
+        signal_df = input_data_df[input_data_df[signal] == 1]
+
+        sequence_ids = signal_df["sequence_id"].tolist()
+        split_idx = int(0.7 * len(sequence_ids))
+        train_ids.extend(sequence_ids[:split_idx])
+        test_ids.extend(sequence_ids[split_idx:])
+
+    train_ids = pd.DataFrame(train_ids, columns=["example_id"])
+    test_ids = pd.DataFrame(test_ids, columns=["example_id"])
+    train_ids.to_csv(Path(f"{output_path}/train_ids.csv"), index=False)
+    test_ids.to_csv(Path(f"{output_path}/test_ids.csv"), index=False)
+
+    input_data_df.to_csv(Path(f"{output_path}/full_simulated_dataset.tsv"), sep="\t", index=False)
+
 
 def main():
     configs_dir = "generative_air_benchmark/configs"
@@ -21,21 +46,21 @@ def main():
     # run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
     #                result_path=f"{output_dir}/00_simulation/")
 
-    # # run training
-    # run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
-    #              result_path=f"{output_dir}/01_train/")
-
-    # run filtering
-    run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
-                 result_path=f"{output_dir}/02_filter/")
-
-    # run feature comparison reports
-    run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
-                 result_path=f"{output_dir}/03_feature_comparison/")
-
-    # run feature value bar plot reports
-    run_immuneML(specification_path=f"{configs_dir}/04_exploratory_analysis_feature_barplot.yaml",
-                 result_path=f"{output_dir}/04_feature_value_barplot/")
+    # run training
+    run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
+                 result_path=f"{output_dir}/01_train/")
+    #
+    # # run filtering
+    # run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
+    #              result_path=f"{output_dir}/02_filter/")
+    #
+    # # run feature comparison reports
+    # run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
+    #              result_path=f"{output_dir}/03_feature_comparison/")
+    #
+    # # run feature value bar plot reports
+    # run_immuneML(specification_path=f"{configs_dir}/04_exploratory_analysis_feature_barplot.yaml",
+    #              result_path=f"{output_dir}/04_feature_value_barplot/")
 
 
 if __name__ == "__main__":
--- generative_air_benchmark/scripts/run_use_case_dummy.py
+++ generative_air_benchmark/scripts/run_use_case_dummy.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+
 from immuneML.app.LigoApp import LigoApp
 from immuneML.app.ImmuneMLApp import ImmuneMLApp
+from run_use_case import get_train_and_test_ids
 
 
 def run_simulation(specification_path, result_path):
@@ -17,25 +19,29 @@ def main():
     configs_dir = "generative_air_benchmark/configs/dummy_configs"
     output_dir = "generative_air_benchmark/dummy_results"
 
-    # # simulate data
-    # run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
-    #                result_path=f"{output_dir}/00_simulation/")
+    # simulate data
+    run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
+                   result_path=f"{output_dir}/00_simulation/")
+
+    # get train and test ids
+    get_train_and_test_ids(input_data_path=f"{output_dir}/00_simulation/dataset/simulated_dataset.tsv",
+                           output_path=f"{output_dir}/00_simulation/")
+
+    # run training
+    run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
+                 result_path=f"{output_dir}/01_train/")
+
+    # # run filtering
+    # run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
+    #              result_path=f"{output_dir}/02_filter/")
     #
-    # # run training
-    # run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
-    #              result_path=f"{output_dir}/01_train/")
-
-    # run filtering
-    run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
-                 result_path=f"{output_dir}/02_filter/")
-
-    # run feature comparison reports
-    run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
-                 result_path=f"{output_dir}/03_feature_comparison/")
-
+    # # run feature comparison reports
+    # run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
+    #              result_path=f"{output_dir}/03_feature_comparison/")
+    #
     # run feature value bar plot reports
-    run_immuneML(specification_path=f"{configs_dir}/04_exploratory_analysis_feature_barplot.yaml",
-                 result_path=f"{output_dir}/04_feature_value_barplot/")
+    run_immuneML(specification_path=f"{configs_dir}/04_exploratory_analyses.yaml",
+                 result_path=f"{output_dir}/04_exploratory_analyses/")
 
 
 if __name__ == "__main__":
