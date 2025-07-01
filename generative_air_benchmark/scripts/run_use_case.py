3b9b3d5409ad6d215e7b56d12671c7bfb6501372
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-07-01 23:50:52 +0200

    update config

--- generative_air_benchmark/scripts/run_use_case.py
+++ generative_air_benchmark/scripts/run_use_case.py
@@ -44,26 +44,26 @@ def main():
     output_dir = "generative_air_benchmark/results"
     training_percentage = 0.7
 
-    # # simulate data
-    # run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
-    #                result_path=f"{output_dir}/00_simulation/")
-    #
-    # # get train and test ids
-    # get_train_and_test_ids(input_data_path=f"{output_dir}/00_simulation/dataset/simulated_dataset.tsv",
-    #                        output_path=f"{output_dir}/00_simulation/",
-    #                        training_percentage=training_percentage)
-    #
-    # # run training
-    # run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
-    #              result_path=f"{output_dir}/01_train/")
+    # simulate data
+    run_simulation(specification_path=f"{configs_dir}/00_simulation.yaml",
+                   result_path=f"{output_dir}/00_simulation/")
 
-    # # run filtering
-    # run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
-    #              result_path=f"{output_dir}/02_filter/")
-    #
-    # # run feature comparison reports
-    # run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
-    #              result_path=f"{output_dir}/03_feature_comparison/")
+    # get train and test ids
+    get_train_and_test_ids(input_data_path=f"{output_dir}/00_simulation/dataset/simulated_dataset.tsv",
+                           output_path=f"{output_dir}/00_simulation/",
+                           training_percentage=training_percentage)
+
+    # run training
+    run_immuneML(specification_path=f"{configs_dir}/01_train.yaml",
+                 result_path=f"{output_dir}/01_train/")
+
+    # run filtering
+    run_immuneML(specification_path=f"{configs_dir}/02_filter.yaml",
+                 result_path=f"{output_dir}/02_filter/")
+
+    # run feature comparison reports
+    run_immuneML(specification_path=f"{configs_dir}/03_exploratory_analysis_feature_comparison.yaml",
+                 result_path=f"{output_dir}/03_feature_comparison/")
 
     # run feature value bar plot reports
     run_immuneML(specification_path=f"{configs_dir}/04_exploratory_analyses.yaml",
